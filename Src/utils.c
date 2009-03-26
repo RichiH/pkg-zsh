8cc181b09280789ca8cdbb6c83a7bf3077700ec1
Clint Adams <schizo@debian.org>
2009-03-25 23:35:05 -0400

    ZW#26625, ZU#13593: inopportune interrupt could wreck terminal set up (combined patch thanks to Vincent Lefevre).  closes: #521174.

--- Src/utils.c
+++ Src/utils.c
@@ -1437,13 +1437,13 @@ settyinfo(struct ttyinfo *ti)
 #  ifndef TCSADRAIN
 #   define TCSADRAIN 1	/* XXX Princeton's include files are screwed up */
 #  endif
-	tcsetattr(SHTTY, TCSADRAIN, &ti->tio);
-    /* if (tcsetattr(SHTTY, TCSADRAIN, &ti->tio) == -1) */
+	while (tcsetattr(SHTTY, TCSADRAIN, &ti->tio) == -1 && errno == EINTR)
+	    ;
 # else
-	ioctl(SHTTY, TCSETS, &ti->tio);
-    /* if (ioctl(SHTTY, TCSETS, &ti->tio) == -1) */
+	while (ioctl(SHTTY, TCSETS, &ti->tio) == -1 && errno == EINTR)
+	    ;
 # endif
-	/*	zerr("settyinfo: %e",errno)*/ ;
+	/*	zerr("settyinfo: %e",errno);*/
 #else
 # ifdef HAVE_TERMIO_H
 	ioctl(SHTTY, TCSETA, &ti->tio);
